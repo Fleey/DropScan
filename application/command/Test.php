69ec72b1c54314fa11edaab2db112da3fde1c9de
Fleey <825190973@qq.com>
2019-05-09 09:10:59 +0800

    push

---
+++ application/command/Test.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace app\command;
+
+use think\console\Command;
+use think\console\Input;
+use think\console\Output;
+use think\Db;
+
+class Test extends Command
+{
+    protected function configure()
+    {
+        // 指令配置
+        $this->setName('test')->setDescription('user test');
+        // 设置参数
+    }
+
+    protected function execute(Input $input, Output $output)
+    {
+        $filePath     = __DIR__ . '/../../public/static/py/resources/Feature.json';
+        $selectResult = Db::name('fingerprint_service')->field('name,type,url,content')->select();
+        file_put_contents($filePath, json_encode($selectResult));
+        // 指令输出
+    }
+
+    private function buildFingerprintService()
+    {
+        $filePath     = __DIR__ . '/../../public/static/py/resources/Feature.json';
+        $selectResult = Db::name('fingerprint_service')->field('name,type,url,content')->where('status', 1)->select();
+        file_put_contents($filePath, json_encode($selectResult));
+    }
+
+}
